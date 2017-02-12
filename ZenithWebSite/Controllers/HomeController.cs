19827bc0f9b25f081927ffab48343cbba7b7f6f4
Arnold Myint <blinkmonster@gmail.com>
2017-02-11 21:18:34 -0800

    homepage working! pepehere!

--- ZenithWebSite/Controllers/HomeController.cs
+++ ZenithWebSite/Controllers/HomeController.cs
@@ -3,14 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZenithDataLib.Models;
+using System.Data.Entity;
 
 namespace ZenithWebSite.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            DateTime mondayDate = DateTime
+                          .Today
+                          .AddDays(((int)(DateTime.Today.DayOfWeek) * -1) + 1);
+
+            DateTime sundayDate = DateTime
+                          .Today
+                          .AddDays(((int)(DateTime.Today.DayOfWeek) * -1) + 8);
+
+            var events = db.Events.Include(e => e.Activity).ToList();
+            events = events.OrderBy(e => e.EventFrom)
+                .Where(e => e.EventFrom >= mondayDate && e.EventFrom <= sundayDate)
+                .ToList();
+
+            var eventsDetail = new Dictionary<string, List<Event>>();
+
+            foreach (var i in events)
+            {
+                if (eventsDetail.ContainsKey(i.EventFrom.ToLongDateString()))
+                {
+                    eventsDetail[i.EventFrom.ToLongDateString()].Add(i);
+                }
+                else
+                {
+                    eventsDetail[i.EventFrom.ToLongDateString()] = new List<Event>();
+                    eventsDetail[i.EventFrom.ToLongDateString()].Add(i);
+                }
+            }
+
+            return View(eventsDetail);
         }
     }
 }
